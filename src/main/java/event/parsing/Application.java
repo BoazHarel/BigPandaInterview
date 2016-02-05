97d164b4d121f7c98cf278f56dc60dc8dd848bc6
BoazHarel <BHarel@VMware.com>
2016-02-05 15:00:17 +0200

    extracted interfaces, added OS support (hopefully)

--- src/main/java/event/parsing/Application.java
+++ src/main/java/event/parsing/Application.java
@@ -1,7 +1,9 @@
 package event.parsing;
 
 import event.parsing.backend.EventProducer;
+import event.parsing.backend.EventProducerImpl;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -20,7 +22,7 @@ public class Application {
     public static void main(String[] args)
                 throws IOException {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
-        EventProducer eventProducer = applicationContext.getBean(EventProducer.class);
+        EventProducer eventProducer = applicationContext.getBean(EventProducerImpl.class);
 
         String pathToExecutable = copyExecutableToTemp();
         ProcessBuilder processBuilder = new ProcessBuilder(pathToExecutable);
@@ -38,9 +40,24 @@ public class Application {
 
     private static String copyExecutableToTemp()
                 throws IOException {
-        InputStream resourceAsStream = Application.class.getResourceAsStream("/generator-windows-amd64.exe");
+        InputStream resourceAsStream = Application.class.getResourceAsStream(geOsSpecificJsonGenerator());
         Path tempFilePath = Files.createTempFile("jsonCreator", "tmp");
         FileUtils.copyInputStreamToFile(resourceAsStream, tempFilePath.toFile());
         return tempFilePath.toString();
+    }
+
+    private static String geOsSpecificJsonGenerator() {
+        String directory = "/jsonGenerators/";
+        if (SystemUtils.IS_OS_WINDOWS) {
+            return directory + "generator-windows-amd64.exe";
+        }
+        if (SystemUtils.IS_OS_LINUX) {
+            return directory + "generator-linux-amd64";
+        }
+        if (SystemUtils.IS_OS_MAC) {
+            return directory + "generator-macosx-amd64";
+        }
+
+        throw new IllegalStateException("Failed to get OS type");
     }
 }
--- src/main/java/event/parsing/backend/EventConsumer.java
+++ src/main/java/event/parsing/backend/EventConsumer.java
@@ -1,66 +1,12 @@
 package event.parsing.backend;
 
-import event.parsing.model.Event;
 import event.parsing.model.EventStats;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.util.Map;
-import java.util.concurrent.ConcurrentSkipListMap;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by bharel on 2/5/2016.
  */
-@Service
-public class EventConsumer {
-
-    @Autowired
-    private EventProducer eventProducer;
-    private SynchronousQueue<Event> events;
-    private ConcurrentSkipListMap<String, EventStats> eventTypeToStats;
-
-    @PostConstruct
-    private void setup(){
-        events = eventProducer.getEvents();
-        eventTypeToStats = new ConcurrentSkipListMap<>();
-        consume();
-    }
-
-    public Map<String, EventStats> getEventTypeToStatsMap() {
-        return eventTypeToStats;
-    }
-
-    private void consume(){
-        Thread thread = new Thread(() -> {
-            while (true){
-                try {
-                    Event event = events.poll();
-                    if(event == null){
-                        Thread.sleep(TimeUnit.SECONDS.toMillis(1));
-                        continue;
-                    }
-
-                    consume(event);
-                } catch (InterruptedException e) {
-                    System.out.println(e);
-                    e.printStackTrace();
-                }
-            }
-        }, EventConsumer.class.getSimpleName());
-        thread.start();
-    }
-
-    private void consume(Event event) {
-        String eventType = event.getType();
-        String eventData = event.getData();
-
-        if(!eventTypeToStats.containsKey(eventType)){
-            eventTypeToStats.put(eventType, new EventStats(eventData));
-        }
-
-        eventTypeToStats.get(eventType).addEventData(eventData);
-    }
+public interface EventConsumer {
+    Map<String, EventStats> getEventTypeToStatsMap();
 }
